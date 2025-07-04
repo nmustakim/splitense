d23c83f9f185380b3c21a4d5cc519109b8ce3b74
nmustakim <nmustakim98@gmail.com>
2025-07-04 16:06:19 +0600

    card theme updated

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -76,7 +76,11 @@ class AppTheme {
         color: Colors.white,
       ),
     ),
-
+    cardTheme: const CardThemeData(
+      elevation: 0,
+      color: darkSurface,
+      surfaceTintColor: Colors.transparent,
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: primaryBlue,
