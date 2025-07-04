9d171da89ab602e396b6dd95939fe28c454e1cb2
nmustakim <nmustakim98@gmail.com>
2025-07-04 16:18:43 +0600

    pumpAndSettle added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,9 +4,12 @@ import 'package:splitense/main.dart';
 
 void main() {
   testWidgets('Splitense app launches successfully', (
-    WidgetTester tester,
-  ) async {
+      WidgetTester tester,
+      ) async {
     await tester.pumpWidget(const Splitense());
+
+    // Wait for all animations and timers to complete
+    await tester.pumpAndSettle(const Duration(seconds: 5));
 
     expect(find.byType(MaterialApp), findsOneWidget);
   });
